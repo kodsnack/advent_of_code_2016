099ceef715a6668001873999e86f5593f95445c6
Emil Andersson <vaccuum@gmail.com>
2016-12-02 16:38:58 +0100

    Day 1, puzzle 2 solution added

--- HerrEmil-JavaScript/day-01-puzzle-01.js
+++ HerrEmil-JavaScript/day-01-puzzle-01.js
@@ -1,19 +1,4 @@
-const initialPosition = [0, 0];
-const initialDirection = [1, 0];
-
-function turn(direction, instruction) {
-	let leftOrRight = instruction.charAt(0);
-
-	if (leftOrRight == 'L') {
-		temp = -direction[0];
-		direction[0] = direction[1];
-		direction[1] = temp;
-	} else {
-		temp = direction[0];
-		direction[0] = -direction[1];
-		direction[1] = temp;
-	}
-}
+let day1 = require('./day-01-common');
 
 
 function move(position, direction, instruction) {
@@ -23,22 +8,17 @@ function move(position, direction, instruction) {
 }
 
 
-function getManhattanDistance(position) {
-	return Math.abs(position[0]) + Math.abs(position[1]);
-}
-
-
 function solve(input) {
 	let instructions = input.split(', ');
-	let direction = initialDirection.slice();
-	let position = initialPosition.slice();
+	let direction = [1, 0];
+	let position = [0, 0];
 
 	for (let i = 0; i < instructions.length; i += 1) {
-		turn(direction, instructions[i]);
+		day1.turn(direction, instructions[i]);
 		move(position, direction, instructions[i]);
 	}
 
-	return getManhattanDistance(position);
+	return day1.getManhattanDistance(position);
 }
 
 
