52b6cb835ce2dddd716203d53075e494c6eb3072
Jonas Thoursie <jonas.thoursie@smhi.se>
2016-12-03 17:51:45 +0100

    Day 3, puzzle 1+2

--- thorseye-typescript/index.ts
+++ thorseye-typescript/index.ts
@@ -7,3 +7,8 @@ console.log(`Day 1, puzzle 2: ${Day1.Puzzle2}`);
 import * as Day2 from './02';
 console.log(`Day 2, puzzle 1: ${Day2.Puzzle1}`);
 console.log(`Day 2, puzzle 2: ${Day2.Puzzle2}`);
+
+
+import * as Day3 from './03';
+console.log(`Day 3, puzzle 1: ${Day3.Puzzle1}`);
+console.log(`Day 3, puzzle 2: ${Day3.Puzzle2}`);
