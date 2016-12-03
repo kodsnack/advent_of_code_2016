139a530d77761b5dc74eb1bdebabbbca688b8414
Anton Bengtsson <anton@antonbengtsson.net>
2016-12-03 09:56:21 +0100

    Day 3 pt. 2

--- clonest1ck-javascript/03dec.js
+++ clonest1ck-javascript/03dec.js
@@ -7,6 +7,7 @@ if(file != undefined) {
   var data = fs.readFileSync(file);
 
   count(data.toString());
+  countColumn(data.toString());
 
 } else {
   console.log("Undefined input, exiting...");
@@ -14,16 +15,49 @@ if(file != undefined) {
 }
 
 function count(input) {
-  var list = input.split("\n");
+  var list = parse(input)
   var triangles = 0;
 
   for(var i = 0; i < list.length - 1; i++) {
-    var dim = list[i].split(" ").sort();
-    dim = dim.splice(dim.length - 3, 3).map(function( num ){ return parseInt( num, 10 ) });
-    dim = dim.sort(function(a, b){return a - b})
+    var dim = list[i].sort(sortNum)
     if(dim[0] + dim[1] > dim[2]) {
       triangles++;
     }
   }
   console.log(triangles);
+}
+
+function countColumn(input) {
+  var list = parse(input);
+  var triangles = 0;
+
+  for(var i = 0; i < list.length - 1; i += 3) {
+    for(var j = 0; j < 3; j++) {
+      var trie = [];
+      for(var k = 0; k < 3; k++) {
+        trie.push(list[i+k][j]);
+      }
+      trie = trie.sort(sortNum);
+      if(trie[0] + trie[1] > trie[2]) {
+        triangles++;
+      }
+    }
+  }
+  console.log(triangles);
+}
+
+function parse(str) {
+  var list = str.split("\n");
+  var res = /\s+/;
+  var rer = /^\s+|\s+$/g;
+
+  for(var i = 0; i < list.length - 1; i++) {
+    var dim = list[i].replace(rer, '').split(res);
+    list[i] = dim.map(function( num ){ return parseInt( num, 10 ) });
+  }
+  return list;
+}
+
+function sortNum(a, b) {
+  return a - b;
 }
