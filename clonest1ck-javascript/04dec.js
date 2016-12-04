1723fee7007d95cd4f7b14b63b44e9bb0f2101b3
Anton Bengtsson <anton@antonbengtsson.net>
2016-12-04 14:50:01 +0100

    Day 4 part 2

--- clonest1ck-javascript/04dec.js
+++ clonest1ck-javascript/04dec.js
@@ -14,19 +14,25 @@ if(file != undefined) {
 }
 
 function realID(inp) {
-  var encr = inp.replace(/-/g, "").split("\n");
+  var encr = inp.split("\n");
   encr.splice(encr.length - 1, 1);
   var sum = 0;
+  var lucky = 0;
 
   for(var str of encr) {
     if(isReal(str)) {
       sum += getID(str);
+      if(isLucky(str)) {
+        lucky = getID(str);
+      }
     }
   }
   console.log(sum);
+  console.log(lucky);
 }
 
 function isReal(str) {
+  str = str.replace(/-/g, "");
   var checksum = str.slice(str.indexOf("["), str.length);
   var dict = {};
   var i = 0;
@@ -45,7 +51,7 @@ function isReal(str) {
   if(max > old) {
     return false;
   }
-  
+
   for(i = 1; i < 6; i++) {
     if(old > dict[checksum[i]] || (old == dict[checksum[i]] && checksum[i - 1] < checksum[i])) {
       old = dict[checksum[i]];
@@ -58,4 +64,38 @@ function isReal(str) {
 
 function getID(str) {
   return parseInt(str.match(/\d+/));
+}
+
+function isLucky(str) {
+  decoded = "";
+
+  shift = getID(str);
+  for(var c of str) {
+    if(c == "-") {
+      decoded += " ";
+    } else if (!isNaN(c)){
+      break;
+    } else {
+      decoded += (shiftChar(c, shift));
+    }
+  }
+  if(decoded.indexOf("northpole object storage") != -1) {
+    return true;
+  } else {
+    //console.log(decoded);
+    return false;
+  }
+}
+
+function shiftChar(char, times) {
+  times = times % 26;
+  charCode = char.charCodeAt() + times;
+  charCodeZ = 'z'.charCodeAt();
+  charCodeA = 'a'.charCodeAt();
+
+  if(charCode > charCodeZ) {
+    return String.fromCharCode(charCodeA + (charCode - charCodeZ) - 1);
+  }
+
+  return String.fromCharCode(charCode);
 }
