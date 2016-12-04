eb6fa2dc9cb58e09914c3394bca432bf82febc17
Peter Westerström <peter.westerstrom@eonreality.com>
2016-12-04 22:29:39 +0000

    Solution for day 4 part 1

---
+++ peterw-cplusplus/Day4/Day4_p2.cpp
@@ -0,0 +1,12 @@
+// Advent of Code 2016 - Day 4 part 2
+// Peter Westerström
+
+#include "config.h"
+
+using namespace std;
+
+int main()
+{
+//	cout << "Day 4 part 2 result: " << validRooms << endl;
+	return 0;
+}
