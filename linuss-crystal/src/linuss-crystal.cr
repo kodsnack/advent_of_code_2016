bfd3363cb9c59ac16b81d24da4c51b4ce21d1ff8
Linus Sellberg <linus@magplus.com>
2016-12-05 16:50:00 +0100

    Day 4.

--- linuss-crystal/src/linuss-crystal.cr
+++ linuss-crystal/src/linuss-crystal.cr
@@ -9,6 +9,8 @@ OptionParser.parse! do |parser|
   parser.on("--2b FNAME", "Day 2b: provide filename") { |name| puts AoC2b.new.process(File.read name).join }
   parser.on("-3 FNAME", "Day 3a: provide filename") { |name| puts AoC3.count_by_row name }
   parser.on("--3b FNAME", "Day 3b: provide filename") { |name| puts AoC3.count_by_column name }
+  parser.on("-4 FNAME", "Day 4: provide filename") { |name| puts AoC4.process_file name }
+  parser.on("--4b FNAME", "Day 4: provide filename") { |name| puts AoC4.north_pole(name).not_nil!.sector }
 
   parser.on("-h", "--help", "Show this help") { puts parser }
 end
