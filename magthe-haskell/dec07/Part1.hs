f84be4eb878a7d509bbce158a1ad0c277f071a97
Magnus Therning <magnus@therning.org>
2016-12-07 07:39:12 +0100

    December 7, Magnus Therning
    Signed-off-by: Magnus Therning <magnus@therning.org>

---
+++ magthe-haskell/dec07/Part1.hs
@@ -0,0 +1,34 @@
+#! /usr/bin/env runhaskell
+
+module Main
+  where
+
+import Control.Arrow
+import System.Environment
+import Text.Parsec as P
+import Text.Parsec.String
+
+readIpList :: FilePath -> IO [([String], [String])]
+readIpList fn = either (const $ error "parse failure") id <$> parseFromFile (many1 parseLine) fn
+
+parseLine :: Parsec String () ([String], [String])
+parseLine = do
+  fs <- parseSupernetSeq
+  ps <- many1 ((,) <$> parseHypernetSeq <*> parseSupernetSeq)
+  optional newline
+  return (fs : map snd ps, map fst ps)
+
+parseHypernetSeq, parseSupernetSeq :: Parsec String () String
+parseHypernetSeq = between (char '[') (char ']') (many1 letter)
+parseSupernetSeq = many1 letter
+
+supportsTLS :: ([String], [String]) -> Bool
+supportsTLS = uncurry (&&) . (any isABBA *** all (not . isABBA))
+  where
+    isABBA l@(a:b:c:d:_) = (a == d && b == c && a /= b) || isABBA (tail l)
+    isABBA _ = False
+
+main :: IO ()
+main = do
+  ips <- head <$> getArgs >>= readIpList
+  print $ length $ filter supportsTLS ips
