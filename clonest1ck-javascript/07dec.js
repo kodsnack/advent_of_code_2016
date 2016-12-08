879ba23e767f5795883ad69e6f15440b2333fe16
Anton Bengtsson <anton@antonbengtsson.net>
2016-12-08 09:54:04 +0100

    Added Day 7 part 2

--- clonest1ck-javascript/07dec.js
+++ clonest1ck-javascript/07dec.js
@@ -6,47 +6,79 @@ if(file != undefined) {
   var fs = require("fs");
   var data = fs.readFileSync(file);
 
-  validIPs(data.toString());
+  var tls = tls(data.toString());
+  var ssl = ssl(data.toString());
+
+  console.log("TLS: " + tls + ". SSL: " + ssl);
 
 } else {
   console.log("Undefined input, exiting...");
   process.exit();
 }
-function validIPs(data) {
+function tls(data) {
   var ok = 0;
   data = data.split("\n");
 
   for(var d of data) {
-    var s = d.split(/[\[\]]/);
-    var isOK = true;
-    var foundInvalid = false;
-    var foundValid = false;
-
-    if(d[0] === "[") {
-      isOK = !isOK;
-    }
-
-    for(var i = 0; i < s.length; i++) {
-      if(isOK && contains(s[i])) {
-        foundValid = foundValid || true;
-      } else if (!isOK && contains(s[i])) {
-        foundInvalid = foundInvalid || true;
-      }
-      isOK = !isOK;
-    }
-
-    if(foundValid && !foundInvalid) {
+    if(validTLS(d)) {
       ok++;
     }
   }
-  console.log(ok);
+  return ok;
 }
 
-function contains(str) {
-  var mat = str.match(/(.)(.)\2\1/);
-  if(mat != null && mat[0].match(/(.)\1\1\1/) == null) {
-    return true;
-  } else {
-    return false;
+function ssl(data) {
+  var ok = 0;
+
+  data = data.split("\n");
+
+  for(var d of data) {
+    if(validSSL(d)) {
+      ok++;
+    }
   }
+  return ok;
+}
+
+function validTLS(str) {
+  var reval   = /(.)(?!\1)(.)\2\1/;
+  var reinval = /\[[^\]\[]*?(.)(?!\1)(.)\2\1[^\]\[]*?\]/;
+
+  if(str.match(reval) != null && str.match(reinval) == null) {
+    return true;
+  }
+  return false;
+}
+
+function validSSL(str) {
+  var withinBrackets = false;
+  var aba = [];
+  var bab = [];
+
+  for(var i = 0; i < str.length - 2; i++) {
+    if(str[i] === "[") {
+      withinBrackets = true;
+    } else if (str[i] === "]") {
+      withinBrackets = false;
+    } else if (str[i] === str[i + 2] && str[i] !== str[i + 1] &&
+                (str[i + 1] !== "]" && str[i + 1] !== "[")) {
+      if(withinBrackets) {
+        bab.push(str[i]+str[i+1]+str[i]);
+      } else {
+        aba.push(str[i+1]+str[i]+str[i+1]); // Invert for faster comparison
+      }
+    }
+  }
+
+  if(aba.length != 0 && bab.length != 0) {
+    for(var i = 0; i < aba.length; i++) {
+      for(var j = 0; j < bab.length; j++) {
+        if(aba[i] === bab[j]) {
+          return true;
+        }
+      }
+    }
+  }
+
+  return false;
 }
