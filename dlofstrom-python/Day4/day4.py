993e4d5965aa78d61d143d36b67736f2a5f9cce1
dlofstrom <lofstrom.daniel@gmail.com>
2016-12-10 22:32:20 +0100

    Day 4 completed

--- dlofstrom-python/Day4/day4.py
+++ dlofstrom-python/Day4/day4.py
@@ -1,10 +1,11 @@
-import re
+import string
 
 input = open('day4-input.txt','r')
 input = [l.strip(']\n').split('[') for l in input]
 
 #Part 1
 sum = 0
+valid = []
 for line in input:
     #Get a list of the letters
     letters = [l for l in list(set(line[0])) if l.isalpha()]
@@ -13,5 +14,18 @@ for line in input:
     #Add to sum if checksum is correct
     if ''.join([c[0] for c in letters[0:5]]) == line[1]:
         sum += int(line[0].split('-')[-1])
+        valid += [line[0]]
 
 print 'Part 1:', sum
+
+#Part 2
+alphabet = string.lowercase
+deciphered = []
+for room in valid:
+    room = room.split('-')
+    shift = int(room[-1])
+    deciphered += [(''.join([' ' if l==' ' else alphabet[(alphabet.find(l)+shift) % len(alphabet)] for l in ' '.join(room[:-1])]), shift)]
+
+for room in deciphered:
+    if not room[0].find('north')==-1:
+        print 'Part 2:', room[1]
