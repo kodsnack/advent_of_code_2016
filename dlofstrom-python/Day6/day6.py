beb9a52ded22b6ca93df4b7bebbd900b219f1cdb
dlofstrom <lofstrom.daniel@gmail.com>
2016-12-10 23:52:21 +0100

    Day 6 completed

--- dlofstrom-python/Day6/day6.py
+++ dlofstrom-python/Day6/day6.py
@@ -3,4 +3,8 @@ input = zip(*[l.strip('\n') for l in input])
 
 #Part 1
 message = ''.join([max(set(col), key=col.count) for col in input])
-print 'Part1:', message
+print 'Part 1:', message
+
+#Part 2
+message = ''.join([min(set(col), key=col.count) for col in input])
+print 'Part 2:', message
