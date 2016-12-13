9667230f54aad74e0199fcc04566ae6cb4760204
Arno Lepisk <arno@lepisk.se>
2016-12-13 19:32:18 +0100

    Refactor solution for day 13.

--- f00ale-cpp/src/p13.cpp
+++ f00ale-cpp/src/p13.cpp
@@ -3,9 +3,11 @@
 #include <deque>
 #include <set>
 
+namespace {
 constexpr int data = 1362;
 constexpr int targetx = 31;
 constexpr int targety = 39;
+constexpr int targetrounds = 50;
 
 bool isWall(int x, int y) {
   int tmp = x*x + 3*x + 2*x*y + y + y*y + data;
@@ -17,26 +19,28 @@ bool isWall(int x, int y) {
   return ret;
 }
 
+}
 
-int main() {
+void p13() {
   std::set<std::tuple<int,int>> visited;
   std::deque<std::tuple<int,int,int>> queue;
 
   visited.insert(std::make_tuple(1,1));
   queue.emplace_back(std::make_tuple(0,1,1));
 
-  bool found = false;
-  int ans = 0;
-  while(!found && !queue.empty()) {
+  int ans1 = 0;
+  int ans2 = 0;
+  while(!queue.empty()) {
     int r = std::get<0>(queue.front());
     int x = std::get<1>(queue.front());
     int y = std::get<2>(queue.front());
     queue.pop_front();
     if(x == targetx && y == targety) {
-      found = true;
-      ans = r;
-      break;
+      ans1 = r;
     }
+
+    if(r <= targetrounds) ans2++;
+    else if(ans1) continue; // stop adding states if answer 1 found and number of rounds reached
 
     for(int dx = -1; dx < 2; dx++) {
       for(int dy = -1; dy < 2; dy++) {
@@ -51,5 +55,10 @@ int main() {
       }
     }
   }
-  std::cout << ans << std::endl;
+  std::cout << ans1 << std::endl;
+  std::cout << ans2 << std::endl;  
+}
+
+int main() {
+  p13();
 }
