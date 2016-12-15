0570d2778079b2fd59ddb3eb1ec9a2ba25727f5c
Arno Lepisk <arno@lepisk.se>
2016-12-15 12:56:37 +0100

    Refactored, more elegant solution for problem 15.

--- f00ale-cpp/src/p15.cpp
+++ f00ale-cpp/src/p15.cpp
@@ -3,35 +3,72 @@
 
 namespace {
 // handparsed data
-constexpr std::array<std::array<int, 2>, 7> discs = {{
+constexpr std::array<std::array<int, 2ul>, 6ul> discs1 = {{
   {{10, 13}},
   {{15, 17}},
   {{17, 19}},
   {{1, 7}},
   {{0, 5}},
-  {{1, 3}},
-  {{0, 11}}
-  }};
+  {{1, 3}}
+}};
 
-int p15_int(const int n) {
-  bool found = false;
-  int t = 0;
-  while(!found) {
-    found = true;
-    for(int i = 0; i < n; i++) {
-      found = found && !((discs[i][0]+t+1+i)%discs[i][1]);
+template<typename T, long unsigned S>
+constexpr std::array<T,S+1> array_append(const std::array<T,S> & arr, const T & e) {
+  std::array<T,S+1> ret{};
+  std::copy(begin(arr), end(arr), begin(ret));
+  ret[S] = e;
+  return ret;
+}
+
+auto discs2 = array_append(discs1, {{0,11}});
+
+template<typename T, long unsigned S>
+constexpr T chinese_remainder(const std::array<std::array<T, 2>, S> & coeff) {
+  T ret = 0;
+  T M = 1;
+  for(auto & a : coeff) M *= a[1];
+
+  std::array<T, S> b = {};
+
+  for(decltype(S) i = 0; i < S; i++) {
+    auto tmp = M / coeff[i][1];
+    for(T bi = 1; bi < coeff[i][1]; bi++) {
+      if(tmp*bi % coeff[i][1] == 1) {
+        b[i] = bi % M;
+        break;
+      }
     }
-    t++;
   }
-  return t-1;
+
+  for(decltype(S) i = 0; i < S; i++) {
+    T tmp = coeff[i][0]*b[i] % M;
+    tmp *= M/coeff[i][1];
+    tmp %= M;
+    ret += tmp;
+    ret %= M;
+  }
+  return ret;
+}
+
+template<typename T, long unsigned int S>
+constexpr T p15_int(const std::array<std::array<T, 2>, S> & coeff) {
+  std::array<std::array<T, 2>, S> mydiscs = coeff;
+
+  // fixup data to chinese remainder theorem compatible format
+  for(decltype(S) i = 0; i < S; i++) {
+    mydiscs[i][0] += (i+1);
+    mydiscs[i][0] = -mydiscs[i][0];
+    while(mydiscs[i][0] < 0) mydiscs[i][0] += mydiscs[i][1];
+    mydiscs[i][0] %= mydiscs[i][1];
+  }
+  return chinese_remainder(mydiscs);
 }
 
 }
 
 void p15() {
-  static_assert(discs.size() == 7, "");
-  std::cout << p15_int(6) << std::endl;
-  std::cout << p15_int(7) << std::endl;
+  std::cout << p15_int(discs1) << std::endl;
+  std::cout << p15_int(discs2) << std::endl;
 }
 
 int main() {
