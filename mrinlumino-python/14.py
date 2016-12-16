daacba40329f783a1c2173c3bc68019d63e146f3
Anders Rosen <anders.rosen@inlumino.se>
2016-12-16 16:07:00 +0100

    Some minor cosmetic fixes

--- mrinlumino-python/14.py
+++ mrinlumino-python/14.py
@@ -40,28 +40,17 @@ while hitcounter < 64:
 	#if len(matches) > 0: print ('String: %s, Match: %s') % (mdFiveString, matches)
 	
 	if len(matches)>0:
-		#print ('Looking for %s') % match
 		# Check the next 1000 hash strings for a sequal of five consecutive chars of the found type
 		for subCounter in range(1000):
 			subMdFiveString = hashlib.md5(puzzleInput + str(counter+subCounter + 1)).hexdigest()
-			#print r'(%s)\1{4}' % match
 			subMatches = re.findall(r'(%s)\1{4}' % matches[0],subMdFiveString)
 			if len(subMatches) > 0:
 				hitcounter += 1
 				print ('FOUND HIT NR: %s on key nr: %s') % (hitcounter, counter)		
 	
-	# Iterate over all possible matches
-#	for match in matches:
-
 	counter += 1
 
-#print ('Hitcounter = %s, Counter = %s') % (hitcounter, counter)
-
-# matches = re.findall(r'(\w)\1{2}', 'jsdfkljsfl000kkkjsddddfhksjhdfiis')
-# for match in matches:
-# 	print match
-
-
+print 'Challenge 1 completed, the 64:th key found'
 
 # ****************************************** challenge 2 ****************************************** 
 
@@ -81,33 +70,33 @@ while hitcounter < 64:
 
 	# Create a MD5 hash from the seed
 	mdFiveString= hashlib.md5(puzzleInputWithNumbers).hexdigest()
+	
+	# Do the stretching
 	for mdCount in range(2016):
 		mdFiveString = 	hashlib.md5(mdFiveString).hexdigest()
 
+	# Look for any character repeated three times in a row
 	matches = re.findall(r'(\w)\1{2}',mdFiveString)
-	#if len(matches) > 0: print ('String: %s, Match: %s') % (mdFiveString, matches)
 	
 	if len(matches)>0:
 		now = time.time()
-		print ('Looking for %s on key nr: %s. Total executeion time: %s') % (matches[0], counter, int(now - start))
 		# Check the next 1000 hash strings for a sequal of five consecutive chars of the found type
 		for subCounter in range(1000):
+		
+			# Create a MD5 hash from the seed
 			subMdFiveString = hashlib.md5(puzzleInput + str(counter+subCounter + 1)).hexdigest()
+			
+			# Do the stretching
 			for mdCount in range(2016):
 			 	subMdFiveString = 	hashlib.md5(subMdFiveString).hexdigest()
-			#print r'(%s)\1{4}' % matches[0]
+			
+			# Look for any character repeated five times in a row
 			subMatches = re.findall(r'(%s)\1{4}' % matches[0],subMdFiveString)
 			if len(subMatches) > 0:
 				hitcounter += 1
 				print ('FOUND HIT NR: %s on key nr: %s') % (hitcounter, counter)		
 	
-	# Iterate over all possible matches
-#	for match in matches:
 
 	counter += 1
 
-#print ('Hitcounter = %s, Counter = %s') % (hitcounter, counter)
-
-# matches = re.findall(r'(\w)\1{2}', 'jsdfkljsfl000kkkjsddddfhksjhdfiis')
-# for match in matches:
-# 	print match
+print 'Challenge 2 completed, the 64:th key found'
