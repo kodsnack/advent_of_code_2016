c548e69394207e1999c2d8bbc4dcd936eb8d5a57
advall <david@advall.se>
2016-12-18 23:53:31 +0100

    Revert "solved day 18 in haskell"
    This reverts commit cf61207dcf94bfe1fc84fc2a23696805b7a33b8e.

--- advall-haskell/test.hs
+++
@@ -1,5 +0,0 @@
-import qualified Data.ByteString.Char8
-import qualified Crypto.Hash.MD5 as MD5
-import Data.ByteString.Base16
-
-main = print $ decode (MD5.hash (Data.ByteString.Char8.pack "apa123"))
