82ef7382592736de64673803a1a4a9eae6214f00
Emil Pirfält <emil.pirfalt@gmail.com>
2016-12-18 17:01:25 +0100

    Structure change for tooling

--- pirfalt-clojure/project.clj
+++ pirfalt-clojure/project.clj
@@ -1,3 +1,4 @@
 (defproject advent_of_code_2016 "0.1.0-SNAPSHOT"
   :description "Advent of code 2016"
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [proto-repl "0.3.1"]])
