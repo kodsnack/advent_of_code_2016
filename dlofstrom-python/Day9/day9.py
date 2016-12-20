fe3b22a1a68b9ada36c090fbe4a6b5b129f41fd5
dlofstrom <lofstrom.daniel@gmail.com>
2016-12-20 23:22:56 +0100

    Day 9 completed

--- dlofstrom-python/Day9/day9.py
+++ dlofstrom-python/Day9/day9.py
@@ -2,6 +2,7 @@ import re
 
 input = open('day9-input.txt','r').read().strip(' \r\n')
 
+#Part 1
 uncompressed = ''
 pattern = '\([0-9]{1,}x[0-9]{1,}\)'
 while True:
@@ -18,5 +19,18 @@ while True:
     if input == '':
         break
 
-
 print 'Part 1:', len(uncompressed)
+
+#Part 2
+input = open('day9-input.txt','r').read().strip(' \r\n')
+#Search for marker and recursively summarize length of decompressed data
+def s(p, w, d):
+    match = re.search(p, d)
+    if match:
+        l, next_w = [int(x) for x in match.group(0)[1:-1].split('x')]
+        sp = re.split(p, d, 1)
+        return w*(s(p, 1, sp[0])+s(p,next_w,sp[1][:l])+s(p,1,sp[1][l:]))
+    else:
+        return w*len(d)
+            
+print 'Part 2:', s(pattern, 1, input)
