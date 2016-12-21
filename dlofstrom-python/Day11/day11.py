dd99d1382117387de922bc59e157ca83400a1c80
dlofstrom <lofstrom.daniel@gmail.com>
2016-12-22 00:56:36 +0100

    Day 11 Part 1 ongoing

--- dlofstrom-python/Day11/day11.py
+++ dlofstrom-python/Day11/day11.py
@@ -4,28 +4,35 @@ input = open('day11-input.txt','r')
 input = [re.split(' contains ', l.strip('\r\n')) for l in input]
 
 #Part 1
-#Each floor contains a list with each item as a tuple (generator,)/(,chip)
-start = [re.findall(r'((?<=a )\w+(?= generator))|((?<=a )\w+(?=-compatible microchip))',l[1]) for l in input]
-elevator = 0
+input = [re.findall(r'((?<=a )\w+(?= generator))|((?<=a )\w+(?=-compatible microchip))',l[1]) for l in input]
+input = ['e0'] + sorted([x[0][0]+'g'+str(i) if x[1]=='' else x[1][0]+'m'+str(i) for i,l in enumerate(input) for x in l])
 
-#State = (elevator, floor contents)
-def pretty_print(state):
-    str = ''
-    for i,s in enumerate(state[1]):
-        floor = ['e'] if state[0]==i else ['']
-        floor.append('')
-        
-        
-        
+def is_valid(state):
+    if int(state[0][-1]) not in range(4):
+        return False
+    gs = [int(x[-1]) for x in state[1::2]]
+    for i,m in enumerate([int(x[-1]) for x in state[2::2]]):
+        if not gs[i]==m and m in gs:
+            return False
+    return True
+            
+def get_possible(state):
+    return [(state[0],)+x for x in list(itertools.combinations(['empty']+[x for x in state[1:] if x[-1]==state[0][-1]],2))]
 
-
-#p = re.compile('+(?<=a )[a-z]{1,}(?=( generator|-compatible microchip))')
-#p = re.compile('((?<=a )[a-z]{1,}(?= generator))((?<=a )[a-z]{1,}(?=-compatible microchip))')
-#for f in floors:
-#    for i in f:
-#        m = re.search('(?<=a )[a-z]{1,}(?=( generator|-compatible microchip))',i)
-#        if m:
-#            print m.group()
+def next_state(state, items, dir):
+    return [x[:-1]+str(int(x[-1])+dir) if x in items else x for x in list(state)]
 
 
+moves = {}
+best = 100000000000000
+def test(state, n):
+    #If done
+    if all([i[-1] == '3' for i in state]):
+        if n < best:
+            best = n
+    else:
+        return 1
+
+
+print is_valid(next_state(input, get_possible(input)[0], 1))
 
