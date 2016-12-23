8a668c2e045bed876310f5284bb5bd04641ea349
Magnus Therning <magnus@therning.org>
2016-12-23 09:37:04 +0100

    December 23, Magnus Therning
    Signed-off-by: Magnus Therning <magnus@therning.org>

---
+++ magthe-haskell/dec23/Part2.hs
@@ -0,0 +1,152 @@
+#! /usr/bin/env stack
+{- stack runghc -}
+
+module Main
+  where
+
+import qualified Data.Sequence as S
+import           System.Environment
+import           Text.Parsec
+import           Text.Parsec.String
+
+safeIndex s i = if i >= S.length s
+                then Nothing
+                else Just $ s `S.index` i
+
+readProgram :: FilePath -> IO [Instruction]
+readProgram fn = either (const $ error "parse failure") id <$> parseFromFile (many1 parseInstr) fn
+
+parseInstr :: Parsec String () Instruction
+parseInstr = choice [try parseCpy, try parseInc, try parseDec, try parseJnz, try parseTgl, try parseMul, try parseNop] <* optional newline
+  where
+    parseCpy = Cpy <$>
+      (string "cpy " *> parseRegOrInt) <*>
+      (char ' ' *> parseReg)
+
+    parseInc = Inc <$> (string "inc " *> parseReg)
+
+    parseDec = Dec <$> (string "dec " *> parseReg)
+
+    parseJnz = Jnz <$>
+      (string "jnz " *> parseRegOrInt) <*>
+      (char ' ' *> parseRegOrInt)
+
+    parseTgl = Tgl <$>
+      (string "tgl " *> parseRegOrInt)
+
+    parseMul = Mul <$>
+      (string "mul " *> parseReg) <*>
+      (char ' ' *> parseReg)
+
+    parseNop = string "nop" >> return Nop
+
+    parseReg = choice [char 'a', char 'b', char 'c', char 'd']
+
+    parseInt = (*) <$>
+      option 1 (char '-' >> return (-1)) <*>
+      (read <$> many1 digit)
+
+    parseRegOrInt = (Left <$> parseReg) <|> (Right <$> parseInt)
+
+data Instruction = Cpy (Either Char Int) Char
+                 | Inc Char
+                 | Dec Char
+                 | Mul Char Char
+                 | Nop
+                 | Jnz (Either Char Int) (Either Char Int)
+                 | JnzT (Either Char Int) Int
+                 | Tgl (Either Char Int)
+                 | TglT Int
+                 deriving (Eq, Show)
+
+type Machine = (Int, Int, Int, Int, Int)
+
+startMachine :: Machine
+startMachine = (0, 12, 0, 0, 0)
+
+getRegValue (pc, rA, rB, rC, rD) r =
+  case r of
+    'p' -> pc
+    'a' -> rA
+    'b' -> rB
+    'c' -> rC
+    'd' -> rD
+
+setRegValue (pc, rA, rB, rC, rD) r v =
+  case r of
+    'p' -> (v, rA, rB, rC, rD)
+    'a' -> (pc, v, rB, rC, rD)
+    'b' -> (pc, rA, v, rC, rD)
+    'c' -> (pc, rA, rB, v, rD)
+    'd' -> (pc, rA, rB, rC, v)
+
+withReg f m r =
+  let v = getRegValue m r
+      m' = setRegValue m r (f v)
+  in m'
+
+constOrReg _ (Right v) = v
+constOrReg m (Left r) = getRegValue m r
+
+type EvalCtx = (S.Seq Instruction, Machine)
+
+evalOne (ins, m) (Cpy (Left s) r) =
+  let s' = getRegValue m s
+      m' = setRegValue m r s'
+  in (ins, withReg succ m' 'p')
+evalOne (ins, m) (Cpy (Right i) r) = (ins, withReg succ (setRegValue m r i) 'p')
+
+evalOne (ins, m) (Inc r) = (ins, withReg succ (withReg succ m r) 'p')
+evalOne (ins, m) (Dec r) = (ins, withReg succ (withReg pred m r) 'p')
+
+evalOne (ins, m) (Mul d r) = (ins, withReg succ (setRegValue m d (t0 * t1)) 'p')
+  where
+    t0 = getRegValue m d
+    t1 = getRegValue m r
+evalOne (ins, m) Nop = (ins, withReg succ m 'p')
+
+evalOne (ins, m) (Jnz p j) =
+  let v = constOrReg m p
+      c = constOrReg m j
+  in if 0 /= v
+     then (ins, withReg (+ c) m 'p')
+     else (ins, withReg succ m 'p')
+evalOne (ins, m) (JnzT _ _) = (ins, withReg succ m 'p')
+
+evalOne im@(_, m) (Tgl (Left r)) = evalOne im (Tgl (Right $ getRegValue m r))
+evalOne (ins, m) (Tgl (Right v)) =
+  let idx = v + getRegValue m 'p'
+      i = ins `safeIndex` idx
+  in flip (maybe (ins, withReg succ m 'p')) i $ \ i' ->
+    case i' of
+    (Inc r) -> (S.update idx (Dec r) ins, withReg succ m 'p')
+    (Dec r) -> (S.update idx (Inc r) ins, withReg succ m 'p')
+    (Tgl (Left c)) -> (S.update idx (Inc c) ins, withReg succ m 'p')
+    (Tgl (Right w)) -> (S.update idx (TglT w) ins, withReg succ m 'p')
+    (TglT _) -> (ins, withReg succ m 'p')
+    (Jnz p (Left c)) -> (S.update idx (Cpy p c) ins, withReg succ m 'p')
+    (Jnz p (Right w)) -> (S.update idx (JnzT p w) ins, withReg succ m 'p')
+    (JnzT p w) -> (S.update idx (Jnz p (Right w)) ins, withReg succ m 'p')
+    (Cpy w c) -> (S.update idx (Jnz w (Left c)) ins, withReg succ m 'p')
+    (Mul _ _) -> error "trying to run 'tgl' on 'mul' is undefined"
+    Nop -> error "trying to run 'tgl' on 'nop' is undefined"
+evalOne im (TglT _) = im
+
+eval ins m =
+  let pl = S.length ins
+      pc = getRegValue m 'p'
+      i = ins `S.index` pc
+      (ins', m') = evalOne (ins, m) i
+  in if pc >= pl
+     then m
+     else eval ins' m'
+
+optimize ins = S.take 2 ins S.>< multIns S.>< S.drop 10 ins
+  where
+    multIns = S.fromList [Mul 'a' 'b', Nop, Nop, Nop, Nop, Nop, Nop, Nop]
+
+main :: IO ()
+main = do
+  program <- (head <$> getArgs) >>= readProgram
+  let (_, a, _, _, _) = eval (optimize $ S.fromList program) startMachine
+  print a
