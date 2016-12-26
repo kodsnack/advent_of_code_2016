622eea4fa48b162130389c9ee80fae08c04da6b6
johanstuff <johan.alm@lsn.se>
2016-12-26 15:26:40 +0100

    Day 8 , Lua
    Day 8 , Lua-script

--- johanstuff-lua/hlp.lua
+++ johanstuff-lua/hlp.lua
@@ -22,20 +22,24 @@ function hlp.split(str, sep)
     if sep == nil then sep = "%s" end
     local t={}   i=1
     for str in string.gmatch(str, "([^"..sep.."]+)") do
-        t[i] = str   i = i + 1  --for k,v in ipairs( split('aa[bb', '[') ) do print(k,v) end
+        t[i] = str   i = i + 1
     end
     return t
 end
 
---Source: Lua manual
+--Source: homebrewed
+function hlp.print(arr)
+    local str =''  for k,v in ipairs(arr) do  str = str..tostring(v)..',' end  print(str)
+end
 function hlp.pprint(arr)
-    --for k,v in ipairs(arr) do print(k,v) end
-    print(table.concat(arr,','))
+    for k,v in ipairs(arr) do
+        local str =''  for kk,vv in ipairs(v) do  str = str..tostring(vv)..',' end  print(str)
+    end
 end
 
 --Source: lua-users.org/wiki
 function hlp.range(from, to, step)
-  step = step or 1
+  --step = step or 1
   return function(_, lastvalue)
     local nextvalue = lastvalue + step
     if step > 0 and nextvalue <= to or step < 0 and nextvalue >= to or
