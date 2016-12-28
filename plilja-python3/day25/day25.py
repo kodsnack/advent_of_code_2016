e47547182b5aa975a4d36e4ea34ccde6f39a3a84
plilja <patlil@kth.se>
2016-12-28 09:34:55 +0100

    Day 25: remove heuristic solution

--- plilja-python3/day25/day25.py
+++ plilja-python3/day25/day25.py
@@ -8,10 +8,21 @@ class Machine():
         self.c = c
         self.d = d
         self.pc = pc
-        self.signal = []
 
 
-def step1(inp):
+    def __hash__(self):
+        return hash((self.a, self.b, self.c, self.d, self.pc))
+
+
+    def __eq__(self, other):
+        return (self.a, self.b, self.c, self.d, self.pc) == (other.a, other.b, other.c, other.d, other.pc)
+
+
+    def copy(self):
+        return Machine(self.a, self.b, self.c, self.d, self.pc)
+
+
+def solve(inp):
     i = 0
     while True:
         copied_list = inp[:]
@@ -22,10 +33,10 @@ def step1(inp):
 
 
 def execute(m, instructions):
+    visited = set()
+    signal = []
     while m.pc < len(instructions):
         xs = instructions[m.pc].strip().split()
-        if try_mul(m, instructions[m.pc:]):
-            continue
         if xs[0] == 'cpy':
             cpy(m, xs[1], xs[2])
         elif xs[0] == 'inc':
@@ -37,66 +48,34 @@ def execute(m, instructions):
         elif xs[0] == 'tgl':
             tgl(m, xs[1], instructions)
         elif xs[0] == 'out':
-            out(m, xs[1])
-            if not valid_signal(m):
+            out(m, signal, xs[1])
+            if not valid_signal(signal):
                 return False
-            if len(m.signal) > 50:
-                # We need a heuristic here since there is no way for a program to solve 
-                # this without being able to solve the halting problem which is impossible.
+            m_copy = m.copy()
+            if (m_copy, tuple(instructions)) in visited:
                 return True
+            visited |= {(m_copy, tuple(instructions))}
         else:
             raise ValueError('Unexpected instruction [%s]' % inp[0])
 
 
-def valid_signal(m):
-    if not m.signal:
+def valid_signal(signal):
+    if not signal:
         return True
-    a = m.signal[0]
+    a = signal[0]
     if a != 0 and a != 1:
         return False
-    for b in m.signal[1:]:
+    for b in signal[1:]:
         if b != (a + 1) % 2:
             return False
         a = b
     return True
 
 
-def out(m, unknown):
+def out(m, signal, unknown):
     v = register_or_constant(m, unknown)
-    m.signal += [v]
+    signal.append(v)
     m.pc += 1
-
-
-def try_mul(m, instructions):
-    if len(instructions) < 6:
-        return False
-    inst0 = instructions[0].strip().split()
-    inst1 = instructions[1].strip().split()
-    inst2 = instructions[2].strip().split()
-    inst3 = instructions[3].strip().split()
-    inst4 = instructions[4].strip().split()
-    inst5 = instructions[5].strip().split()
-    if inst0[0] == 'cpy' and \
-            inst1[0] == 'inc' and \
-            inst2[0] == 'dec' and \
-            inst3[0] == 'jnz' and \
-            inst4[0] == 'dec' and \
-            inst5[0] == 'jnz' and \
-            register_or_constant(m, inst3[2]) == -2 and \
-            register_or_constant(m, inst5[2]) == -5 and \
-            inst0[2] == inst3[1] and \
-            inst2[1] == inst3[1] and \
-            inst4[1] == inst5[1] and \
-            len({inst1[1],  inst2[1], inst4[1]}) == 3 and \
-            get_register(m, inst0[2]) >= 0 and \
-            get_register(m, inst4[1]) >= 0:
-        m.pc += 5
-        set_register(m, inst1[1], get_register(m, inst1[1]) + (register_or_constant(m, inst0[1]) * get_register(m, inst4[1])))
-        set_register(m, inst2[1], 0)
-        set_register(m, inst4[1], 0)
-        return True
-        
-    return False
 
 
 def register_or_constant(machine, unknown):
@@ -185,4 +164,4 @@ def set_register(machine, register, value):
 
 
 inp = sys.stdin.readlines()
-print(step1(inp))
+print(solve(inp))
