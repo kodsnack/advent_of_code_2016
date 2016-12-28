6f22dded9d2c6842b091f42e401758df502513f7
niklasad1 <niklasadolfsson1@gmail.com>
2016-12-28 02:22:30 +0100

    partA done

--- niklasad1-rust/day4/src/main.rs
+++ niklasad1-rust/day4/src/main.rs
@@ -12,10 +12,9 @@ use std::io::prelude::*;
 use std::io::BufReader;
 use std::path::Path;
 use std::ascii::AsciiExt;
-use std::collections::HashMap;
-use std::collections::hash_map::Entry;
 use regex::Regex;
 use std::iter::FromIterator;
+use std::collections::VecDeque;
 
 fn main()
 {
@@ -31,6 +30,7 @@ fn main()
         let mut crc: &str = "";
         let mut letter_count = vec![0; 26];
 
+        /* group chars, as "x-y-z-" 123 [abcde] */
         let re = Regex::new(r"(?P<name>[[:alpha:]-]+)(?P<seq>[\d]{3})\[(?P<crc>[[:alpha:]]+)\]").unwrap();
 
         for cap in re.captures_iter(&line)
@@ -39,7 +39,6 @@ fn main()
             crc = cap.name("crc").unwrap_or("");
             seq = cap.name("seq").unwrap_or("").parse().unwrap();
         }
-        print!("{} {} {}\n", name, seq, crc);
         for c in name.chars()
         {
             let ch = c.to_ascii_lowercase();
@@ -70,32 +69,60 @@ where P: AsRef<Path>,
           buf.lines().map(|l| l.expect("Could not parse line")).collect()
       }
 
-fn create_empty_hash_table() -> HashMap<char, u8>
-{
-    let mut letters_count = HashMap::new();
-    for i in 0..26
-    {
-        let c: u8 = i + 97;
-        letters_count.insert(c as char, 0);
-    }
-    letters_count
-}
-
-
 /*
  * This function is not elegant  but it's pretty straight-forward
  *
- * 1. add the top-5 letters to a hash table where higher rank's are stored as keys, i.e. 0 highest, 1
+ * 1. add the top-5 letters to a fifi queue  where higher rank's are stored as keys, i.e. 0 highest, 1
  *    second highest .... and so on but several letters can have the same rank.
- * 2. determine whether the crc is valid by popping elements from the hashmap
+ * 2. determine whether the crc is valid by popping elements from the queue
  *
  */
 fn valid_crc(crc: &str, letters: &mut Vec<isize>) -> bool
 {
-    let mut rank: HashMap<isize, Vec<char>> = HashMap::new();
-    for i in 0..5
+
+    let mut queue = sort_hash_map(letters);
+    let mut crc_count = 0;
+    let mut exp = 0;
+    let mut invalid = false;
+    while !queue.is_empty() && !invalid
+    {
+        let s = match queue.pop_front()
+        {
+            Some(value) => String::from_iter(value),
+            None => panic!("not possible"),
+        };
+        let len = s.len();
+        exp = if exp + len <= 4 {exp+len} else {4};
+        if exp > crc_count
+        { 
+            for _ in 0..exp-crc_count
+            {
+                for c in s.chars()
+                {
+                    if c == crc.chars().nth(crc_count).unwrap() { crc_count += 1; break;}
+                }
+            }
+        }
+        if exp != crc_count {invalid=true;}
+    }
+    let ret = if crc_count == 4 {true} else {false};
+    ret
+}
+
+fn usize_to_char(v: usize) -> char
+{
+    let dummy = (v+97) as u8;
+    let ch = dummy as char;
+    ch
+}
+
+fn sort_hash_map(letters: &mut Vec<isize>) -> VecDeque<Vec<char>>
+{
+    let mut queue: VecDeque<Vec<char>> = VecDeque::new();
+    for _ in 0..5
     {
         /* find the first biggest */
+        let mut vec = Vec::new();
         let mut biggest = (0, 0);
         for j in 0..letters.len()
         {
@@ -113,58 +140,18 @@ fn valid_crc(crc: &str, letters: &mut Vec<isize>) -> bool
         };
 
         letters[biggest.0] = -1;
-        rank.insert(i, vec![usize_to_char(biggest.0)]);
+        vec.push(usize_to_char(biggest.0));
 
         /* find elements with equal frequency */
         for j in 0..letters.len()
         {
             if biggest.1 == letters[j] && biggest.0 != j
             {
-                match rank.entry(i as isize)
-                {
-                    Entry::Occupied(mut e) => e.get_mut().push(usize_to_char(j)),
-                    _ => panic!("error should not happen"),
-                }
+                vec.push(usize_to_char(j));
                 letters[j] = -1;
             }
         }
+        queue.push_back(vec);
     }
-
-    print!("{:?}\n", rank);
-
-    let mut index = 0;
-    let mut crc_count = 0;
-    while !rank.is_empty() && crc_count < 4
-    {
-        let s = match rank.remove(&index)
-        {
-            Some(value) => String::from_iter(value),
-            None => panic!("\n"),
-        };
-        
-        let mut max = s.len(); 
-        while crc_count < 4 && max > 0
-        {
-            for c in s.chars()
-            {
-                if c == crc.chars().nth(crc_count).unwrap()
-                {
-                    crc_count += 1;
-                    break;
-                }
-            }
-            max -= 1;
-        }
-        index += 1;
-    }
-    print!("crc_count {} crc {}\n", crc_count, crc);
-    let ret = if crc_count == 4 {true} else {false};
-    ret
-}
-
-fn usize_to_char(v: usize) -> char
-{
-    let dummy = (v+97) as u8;
-    let ch = dummy as char;
-    ch
+    queue
 }
