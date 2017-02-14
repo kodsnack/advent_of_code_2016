d71c3f662a3f8647ab51b84e6dcb01a52609e3fa
dlofstrom <lofstrom.daniel@gmail.com>
2017-02-14 17:09:52 +0100

    Day 12 part 1 ongoing

---
+++ dlofstrom-python/Day12/day12.py
@@ -0,0 +1,20 @@
+
+input = open('day12-input.txt','r')
+input = [l.strip('\r\n').split(' ') for l in input]
+
+def cpy(xy):
+    print 'cpy',xy
+
+def inc(x):
+    print 'inc',x
+
+def dec(x):
+    print 'dec',x
+
+def jnz(xy):
+    print 'jnx',xy
+
+
+def assembunny_eval(xy):
+    print 'assembunny_eval'
+
