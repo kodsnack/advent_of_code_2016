13b856379fc10563a57fa309b86a7f2adc38b0ad
dlofstrom <lofstrom.daniel@gmail.com>
2017-02-15 16:20:49 +0100

    Day 14 completed

--- dlofstrom-python/Day14/day14.py
+++ dlofstrom-python/Day14/day14.py
@@ -3,20 +3,32 @@ import re
 
 input = 'jlmsuwbz'
 
-i3 = 0
-keys = []
-while len(keys)<64:
-    h3 = md5(input+str(i3)).hexdigest()
-    m3 = re.findall(r'((\w)\2\2)',h3)
-    if m3:
-        for i5 in range(i3,i3+1000):
-            h5 = md5(input+str(i5)).hexdigest()
-            m5 = re.findall(r'((\w)\2\2\2\2)',h5)
-            if m5:
-                if m3[0][1] in [m[0][1] for m in m5] and not i3==i5:
-                    keys += [i3]
-                    break
-    #print index, hash
-    i3 += 1
+#Pre generate hash, key stretch n times
+def generate_hash(s,n):
+    h = [s+str(i) for i in range(100000)]
+    for i in range(n):
+        h = [md5(x).hexdigest() for x in h]
+    return h
 
-print 'Part 1:', keys[-1]
+#Find keys
+def find_keys(hash):
+    i3 = 0
+    keys = []
+    while len(keys)<64:
+        h3 = hash[i3]
+        m3 = re.findall(r'((\w)\2\2)',h3)
+        if m3:
+            for i5 in range(i3,i3+1000):
+                h5 = hash[i5]
+                m5 = re.findall(r'((\w)\2\2\2\2)',h5)
+                if m5:
+                    if m3[0][1] in [m[0][1] for m in m5] and not i3==i5:
+                        keys += [i3]
+                        break
+        i3 += 1
+    return keys[-1]
+
+
+print 'Part 1:', find_keys(generate_hash(input, 1))
+print 'Part 2:', find_keys(generate_hash(input, 2017))
+
