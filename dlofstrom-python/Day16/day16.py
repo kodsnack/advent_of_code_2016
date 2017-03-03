ee5ea3df547feb378127487397350103e7ae8c6a
dlofstrom <lofstrom.daniel@gmail.com>
2017-03-03 16:16:36 +0100

    Day 16 completed

--- dlofstrom-python/Day16/day16.py
+++ dlofstrom-python/Day16/day16.py
@@ -1,13 +1,13 @@
 input = '01111010110010011'
 
-disk_length = 272
-data = input
-while len(data) < disk_length:
-    data = data + '0' + ''.join(['0' if b=='1' else '1' for b in data[::-1]])
-
-checksum = data[:disk_length]
-while len(checksum) % 2 == 0:
-    checksum = ''.join(['1' if i==j else '0' for i,j in zip(checksum[0::2],checksum[1::2])])
-
-print 'Part 1:', checksum
-
+def generate_checksum(disk_length, data):
+    while len(data) < disk_length:
+        data = data + '0' + ''.join(['0' if b=='1' else '1' for b in data[::-1]])
+        
+    checksum = data[:disk_length]
+    while len(checksum) % 2 == 0:
+        checksum = ''.join(['1' if i==j else '0' for i,j in zip(checksum[0::2],checksum[1::2])])
+    return checksum
+            
+print 'Part 1:', generate_checksum(272, input)
+print 'Part 2:', generate_checksum(35651584, input)
