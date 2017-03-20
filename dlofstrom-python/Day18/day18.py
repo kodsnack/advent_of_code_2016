df0f17fe28ceac1d2ccd7e1baeb30b0378a81006
dlofstrom <lofstrom.daniel@gmail.com>
2017-03-20 14:07:23 +0100

    Day 18 completed

--- dlofstrom-python/Day18/day18.py
+++ dlofstrom-python/Day18/day18.py
@@ -1,10 +1,14 @@
 input = open('day18-input.txt','r')
-input = [r.strip('\n') for r in input]
+input = input.read().strip('\n')
 
-for i in range(40-1):
-    row = '.'+input[-1]+'.'
-    row = [row[i:i+3] for i in range(len(row)-2)]
-    input.append(''.join(['^' if t in ['^..','..^','^^.','.^^'] else '.' for t in row]))
+def count_safe_tiles(row1, n):
+    rows = [row1]
+    for i in range(n-1):
+        row = '.'+rows[-1]+'.'
+        row = [row[i:i+3] for i in range(len(row)-2)]
+        rows.append(''.join(['^' if t in ['^..','..^','^^.','.^^'] else '.' for t in row]))
+    return ''.join(rows).count('.')
     
-print 'Part 1:', ''.join(input).count('.')
+print 'Part 1:', count_safe_tiles(input,40)
+print 'Part 2:', count_safe_tiles(input,400000)
 
