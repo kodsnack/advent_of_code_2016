4c36867fb5d4bfd6a786aab0a6e1d6acd9da69cb
dlofstrom <lofstrom.daniel@gmail.com>
2017-03-20 13:42:04 +0100

    Day 17 completed

--- dlofstrom-python/Day17/day17.py
+++ dlofstrom-python/Day17/day17.py
@@ -1,30 +1,34 @@
 from hashlib import md5
-import re
 
 input = 'gdjjyniy'
 
+def get_next_paths(cp):
+    ch = list(md5(input+cp).hexdigest()[0:4])
+    np = [cp+['U','D','L','R'][i] for i,e in enumerate(ch) if e in 'bcdef']
+    return [n for n in np if (sum([-1 if x=='L' else 1 if x=='R' else 0 for x in n]) in range(4) and sum([-1 if x=='U' else 1 if x=='D' else 0 for x in n]) in range(4))]
+
+def is_goal(p):
+    return [sum(a) for a in zip(*[(-1,0) if x=='L' else (1,0) if x=='R' else (0,-1) if x=='U' else (0,1) if x=='D' else (0,0) for x in p])] == [3,3]
+
+#Part 1
 path_queue = ['']
-
-count = 0
-
 while path_queue:
-    #Pop path from bfs queue
     current_path = path_queue.pop(0)
+    if is_goal(current_path):
+        print 'Part 1:', current_path
+        break
+    path_queue += get_next_paths(current_path)
 
-    #Hash current path to get reachable rooms
-    current_hash = list(md5(input+current_path).hexdigest()[0:4])
 
-    #Add open doors to queue if position in room is valid
-    next_paths = [current_path+['U','D','L','R'][i] for i,e in enumerate(current_hash) if e in 'bcdef']
-    for n in next_paths:
-        n_position = (sum([-1 if x=='L' else 1 if x=='R' else 0 for x in n]), sum([-1 if x=='U' else 1 if x=='D' else 0 for x in n]))
-        if n_position == (3,3):
-            print 'Part 1:', n
-            path_queue = []
-            break
-        elif n_position[0] in range(4) and n_position[1] in range(4):
-            path_queue.append(n)
-    
-        
-    
+#Part 2
+path_queue = ['']
+path_lengths = []
+while path_queue:
+    current_path = path_queue.pop(0)
+    if is_goal(current_path):
+        path_lengths.append(len(current_path))
+    else:
+        path_queue += get_next_paths(current_path)
+print 'Part 2:', max(path_lengths)
+
 
