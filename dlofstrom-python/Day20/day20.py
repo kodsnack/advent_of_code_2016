624ac3b9bc4e567e54c853086d218a5e5493bb69
dlofstrom <lofstrom.daniel@gmail.com>
2017-03-21 13:56:16 +0100

    Day 20 completed

--- dlofstrom-python/Day20/day20.py
+++ dlofstrom-python/Day20/day20.py
@@ -2,11 +2,24 @@ input = open('day20-input.txt','r')
 input = [[int(i) for i in l.strip('\n').split('-')] for l in input]
 input.sort(key=lambda x: x[0])
 
-#Part 1
-allowed = 0
+#Generate a non overlapping list of blocked IP ranges
+no = []
+r = [0,0]
 for l in input:
-    if l[0] <= allowed:
-        allowed = l[1]+1
+    print l
+    if l[0] <= r[1]+1:
+        r[1] = max(l[1],r[1])
     else:
-        break
-print 'Part 1:', allowed
+        print 'adding', r
+        no.append(r)
+        r = l
+no.append(r)
+
+#Generate list of valid IPs
+valid_ips = []
+for i in range(len(no)-1):
+    valid_ips += range(no[i][1]+1, no[i+1][0])
+
+
+print 'Part 1:', valid_ips[0]
+print 'Part 2:', len(valid_ips)
