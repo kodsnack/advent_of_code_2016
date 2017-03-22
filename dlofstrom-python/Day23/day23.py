cc45537b49a3a64f74520ec3f8da384d57ce4614
dlofstrom <lofstrom.daniel@gmail.com>
2017-03-22 21:38:02 +0100

    Day 23 completed

--- dlofstrom-python/Day23/day23.py
+++ dlofstrom-python/Day23/day23.py
@@ -5,8 +5,21 @@ input = [l.strip('\r\n').split(' ',1) for l in input]
 registers = {'a':7, 'b':0, 'c':0, 'd':0}
 
 def cpy(xy,i):
-    #print 'cpy', xy
     x, y = xy.split(' ')
+
+    #Detect multiply case
+    if i+5 < len(input):
+        if input[i+1][0] == 'inc' and input[i+2][0] == 'dec' and input[i+3][0] == 'jnz' and input[i+4][0] == 'dec' and input[i+5][0] == 'jnz':
+            if input[i+2][1] == input[i+3][1][0] and input[i+4][1] == input[i+5][1][0]:
+                if x.isdigit():
+                    registers[input[i+1][1]] += int(x)*registers[input[i+4][1]]
+                else:
+                    registers[input[i+1][1]] += registers[x]*registers[input[i+4][1]]
+                registers[input[i+2][1]] = 0
+                registers[input[i+4][1]] = 0
+                return 6
+
+    #print 'cpy', xy    
     if y not in registers:
         return 1
     if x in registers:
@@ -43,8 +56,6 @@ toggle_table = {'inc':'dec','dec':'inc','tgl':'inc','jnz':'cpy','cpy':'jnz'}
 
 def tgl(x,i):
     #print 'tgl', x
-    global input
-    global toggle_table
     toggle = i + registers[x]
     if toggle >= len(input):
         return 1
@@ -54,10 +65,17 @@ def tgl(x,i):
 def assembunny_eval(reg):
     i = 0
     while True:
-        #print i,registers
+        #print i, input[i],registers
         i += eval(input[i][0]+'(\''+input[i][1]+'\',i)')
         if not i in range(len(input)):
             break
     return registers[reg]
+
 print 'Part 1:', assembunny_eval('a')
 
+input = open('day23-input.txt','r')
+input = [l.strip('\r\n').split(' ',1) for l in input]
+
+registers = {'a':12, 'b':0, 'c':0, 'd':0}
+
+print 'Part 2:', assembunny_eval('a')
